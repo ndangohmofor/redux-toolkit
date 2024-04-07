834f302cb9049bfb814da1029f613ae64f3ed446
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-07 00:10:09 -0400

    implement responsibilities of the redux store - create store, expose getState, subscribe to state, dispatch action to update state, and unsubscribe from state

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const redux = require("redux");
+
+const createStore = redux.createStore;
+
 const CAKE_ORDERED = "CAKE_ORDERED";
 
 function orderCake(qty = 1) {
@@ -21,3 +25,15 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("Initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated state", store.getState())
+);
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+unsubscribe();
