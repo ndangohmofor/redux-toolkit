21cde2f38ad35991f0f45f6955c46556832ab2c8
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-09 06:31:49 -0400

    call the fetchUser function on the store

--- asyncActions.js
+++ asyncActions.js
@@ -86,4 +86,4 @@ const store = createStore(reducer, applyMiddleware(thunkMiddleware));
 store.subscribe(() => {
   console.log(store.getState());
 });
-store.dispatch(fetchUsers);
+store.dispatch(fetchUsers());
