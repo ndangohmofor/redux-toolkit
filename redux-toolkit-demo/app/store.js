9b91e9926a79d7834c40c27facd0f8b6ba2a0fbd
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-11 04:52:20 -0400

    add icecream reducer into the store

--- redux-toolkit-demo/app/store.js
+++ redux-toolkit-demo/app/store.js
@@ -1,8 +1,12 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const cakeReducer = require("../features/cake/cakeSlice");
+const icecreamReducer = require("../features/icecream/icecreamSlice");
 
 const store = configureStore({
-  reducer: { cake: cakeReducer },
+  reducer: {
+    cake: cakeReducer,
+    iceCream: icecreamReducer,
+  },
 });
 
 module.exports = store;
