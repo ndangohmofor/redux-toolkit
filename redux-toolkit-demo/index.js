8fd81b83744c998c6771e495bc7dd46a02708a15
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-12 06:55:09 -0400

    uncomment and dispatch all the actions in the store

--- redux-toolkit-demo/index.js
+++ redux-toolkit-demo/index.js
@@ -9,13 +9,13 @@ const unsubscribe = store.subscribe(() => {});
 
 store.dispatch(fetchUsers());
 
-// store.dispatch(cakeActions.ordered());
-// store.dispatch(cakeActions.ordered());
-// store.dispatch(cakeActions.ordered());
-// store.dispatch(cakeActions.restocked(3));
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.restocked(3));
 
-// store.dispatch(icecreamActions.ordered());
-// store.dispatch(icecreamActions.ordered());
-// store.dispatch(icecreamActions.restocked(2));
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(2));
 
-// unsubscribe();
+unsubscribe();
