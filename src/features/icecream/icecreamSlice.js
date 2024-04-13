b43b33968c7c7581eb76cb6634c578559d886189
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-13 07:28:25 -0400

    import the actions, reducers and stores from reduxtoolkit project and change the import/export statements to ES6 format

---
+++ src/features/icecream/icecreamSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { cakeActions } from "../cake/cakeSlice";
+
+const initialState = {
+  numOfIcecreams: 20,
+};
+
+const icecreamSlice = createSlice({
+  name: "icecream",
+  initialState,
+  reducers: {
+    ordered: (state) => {
+      state.numOfIcecreams--;
+    },
+
+    restocked: (state, action) => {
+      state.numOfIcecreams += action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(cakeActions.ordered, (state) => {
+      state.numOfIcecreams--;
+    });
+  },
+});
+
+export default icecreamSlice.reducer;
+export const { ordered, restocked } = icecreamSlice.actions;
