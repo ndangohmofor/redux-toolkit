f7609eccb383d6d8df4076a8f7dabdd783723f5d
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-13 22:13:47 -0400

    add input element to enable user input the number of ice creams to restock

--- src/features/icecream/icecreamView.jsx
+++ src/features/icecream/icecreamView.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { ordered, restocked } from "./icecreamSlice";
 
 const IcecreamView = () => {
+  const [value, setValue] = useState(1);
   const dispatch = useDispatch();
   const numOfIcecreams = useSelector((state) => state.iceCream.numOfIcecreams);
   return (
     <div>
       <h2>Number of ice creams - {numOfIcecreams}</h2>
       <button onClick={() => dispatch(ordered())}>Order ice creams</button>
-      <button onClick={() => dispatch(restocked(10))}>
+      <input
+        type="number"
+        value={value}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+      />
+      <button onClick={() => dispatch(restocked(value))}>
         Restock ice creams
       </button>
     </div>
