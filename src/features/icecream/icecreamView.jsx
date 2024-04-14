e78558cfbfd8b79392c7ce16e68c91190aad4245
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-13 22:20:19 -0400

    update ice cream view for better display

--- src/features/icecream/icecreamView.jsx
+++ src/features/icecream/icecreamView.jsx
@@ -9,15 +9,21 @@ const IcecreamView = () => {
   return (
     <div>
       <h2>Number of ice creams - {numOfIcecreams}</h2>
-      <button onClick={() => dispatch(ordered())}>Order ice creams</button>
-      <input
-        type="number"
-        value={value}
-        onChange={(e) => setValue(parseInt(e.target.value))}
-      />
-      <button onClick={() => dispatch(restocked(value))}>
-        Restock ice creams
-      </button>
+      <div>
+        <button onClick={() => dispatch(ordered())}>Order ice creams</button>
+      </div>
+      <div>
+        <div>
+          <input
+            type="number"
+            value={value}
+            onChange={(e) => setValue(parseInt(e.target.value))}
+          />
+        </div>
+        <button onClick={() => dispatch(restocked(value))}>
+          Restock ice creams
+        </button>
+      </div>
     </div>
   );
 };
