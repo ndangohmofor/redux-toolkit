91119b9302d594d5e9191e4ea342c41586bc73ef
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-13 21:52:21 -0400

    useSelector to access the state from store and display in the num of icecreams in view

--- src/features/icecream/icecreamView.jsx
+++ src/features/icecream/icecreamView.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const IcecreamView = () => {
+  const numOfIcecreams = useSelector((state) => state.iceCream.numOfIcecreams);
   return (
     <div>
-      <h2>Number of ice creams - </h2>
+      <h2>Number of ice creams - {numOfIcecreams}</h2>
       <button>Order ice creams</button>
       <button>Restock ice creams</button>
     </div>
