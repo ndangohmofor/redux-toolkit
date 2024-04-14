9414e9d90de6e73da3ea6db1b302f8e28e9e3225
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-13 22:35:56 -0400

    display users in the user view

--- src/features/user/userView.jsx
+++ src/features/user/userView.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUsers } from "./userSlice";
 
 const UserView = () => {
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
   return (
     <div>
       <h2>List of Users</h2>
+      {users && !users.loading ? (
+        <ul>
+          {users.users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <h3>Loading...</h3>
+      )}
     </div>
   );
 };
